eec0a0e0057cb6a662a18845d2bbe5bab81512a7
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-02-26 23:10:16 -0300

    typing Knex tables

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+Knex
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
+  }
+}
