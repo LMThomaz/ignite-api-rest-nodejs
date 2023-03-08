e18d1fb7376eb54fdabfb091f4f1862a563c9dc5
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-03-08 08:19:39 -0300

    category tests

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
